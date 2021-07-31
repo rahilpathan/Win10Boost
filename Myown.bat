76ad4b948cdf34cc0a434aa1b84d5606bceb25a8
rahilpathan <rahilpathan56@gmail.com>
2021-07-30 23:34:01 -0400

    Create Myown.bat

---
+++ Myown.bat
@@ -0,0 +1,19 @@
+:: REMOVE BLOATWARES
+Get-AppxPackage *Microsoft.YourPhone* -AllUsers | Remove-AppxPackage.
+
+
+
+::REMOVE TELEMETRY
+
+
+
+
+::INTERNET TWEAKS
+
+
+
+
+::SYSTEM TWEAKS FOLDERS/WIN UPDATES
+
+
+
