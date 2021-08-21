c378b4d1977f8d16de1bc78e6fbfad6b87a8c18c
rahilpathan <rahilpathan56@gmail.com>
2021-08-21 16:30:42 -0400

    Update TheBasictweaksv1.0.bat

--- TheBasictweaksv1.0.bat
+++ TheBasictweaksv1.0.bat
@@ -229,7 +229,7 @@ sc config WpcMonSvc start= disabled
 ::reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Spynet" /v "SpyNetReporting" /t REG_DWORD /d 0 /f
 ::reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Spynet" /v "SubmitSamplesConsent" /t REG_DWORD /d 0 /f
 
-::DISABLE WINDOWS DEFENDER
+::DISABLE WINDOWS DEFENDER (DISABLED AS FLAGGED BY SOME AV as MALWR)
 ::net stop WinDefend
 ::net stop WdNisSvc
 ::sc config WinDefend start= disabled (DISABLED AS FLAGGED BY SOME AV as MALWR)
