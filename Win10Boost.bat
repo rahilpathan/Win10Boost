9e27f06d2442962bf46d0d390ff5339fdefee2f0
rahilpathan <rahilpathan56@gmail.com>
2021-09-19 23:24:11 -0400

    Update Win10Boost.bat

--- Win10Boost.bat
+++ Win10Boost.bat
@@ -196,6 +196,10 @@ reg add "HKCU\Control Panel\Keyboard" /v "InitialKeyboardIndicators" /t REG_SZ /
 reg add "HKEY_USERS\.DEFAULT\Control Panel\Keyboard" /v "KeyboardDelay" /t REG_SZ /d "0" /f
 reg add "HKEY_USERS\.DEFAULT\Control Panel\Keyboard" /v "KeyboardSpeed" /t REG_SZ /d "31" /f
 
+::REMOVE SHORTCUT TEXT FROM SHORTCUTS (s:tenforums.com)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\NamingTemplates" /v "ShortcutNameTemplate" /t REG_SZ /d "\"%%s.lnk\"" /f 1>NUL 2>NUL
+
+
 
 :::::: GAMING TWEAKS ::::::
 
