697478e2edc34f2576bfe1a7eaabcc2452ae7c86
rahilpathan <rahilpathan56@gmail.com>
2021-09-24 07:40:42 -0400

    Update Win10Boost.bat

--- Win10Boost.bat
+++ Win10Boost.bat
@@ -370,12 +370,6 @@ icacls "%WinDir%\System32\bcastdvr.exe" /grant:r Administrators:F /c
 taskkill /im bcastdvr.exe /f
 move C:\Windows\System32\bcastdvr.exe C:\Windows\System32\bcastdvr.OLD
 
-::OTHER TWEAKS
-reg add "HKLM\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "FeatureTestControl" /t REG_DWORD /d 0x0000ffff /f
-
-
-:::::::: PERFORMANCE TWEAKS ::::::::
-
 ::COMPLETELY REMOVING CORTANA
 Powershell -Command "Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "BingSearchEnabled" /t REG_DWORD /d "0" /f
@@ -412,6 +406,15 @@ reg add "HKLM\Software\Policies\Microsoft\Windows\Windows Search" /v "ConnectedS
 reg add "HKLM\Software\Policies\Microsoft\Windows\Windows Search" /v "DisableWebSearch" /t REG_DWORD /d "1" /f
 reg add "HKLM\Software\Policies\Microsoft\Windows\Windows Search" /v "DoNotUseWebResults" /t REG_DWORD /d "1" /f
 
+::OTHER TWEAKS
+reg add "HKLM\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "FeatureTestControl" /t REG_DWORD /d 0x0000ffff /f
+
+
+
+:::::::: PERFORMANCE TWEAKS ::::::::
+
+
+
 ::POWER BOOSTER
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 95533644-e700-4a79-a56c-a89e8cb109d9
 powercfg -changename 95533644-e700-4a79-a56c-a89e8cb109d9 EXTREME-SPEED
@@ -434,31 +437,44 @@ Powercfg -setactive scheme_current
 Powercfg -setdcvalueindex scheme_current sub_processor PROCTHROTTLEMIN 30
 Powercfg -setactive scheme_current
 :: Disable Away Mode policy
-powercfg /setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 25dfa149-5dd1-4736-b5ab-e8a37b5b8187 0
+powercfg -setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 25dfa149-5dd1-4736-b5ab-e8a37b5b8187 0
 Powercfg -setactive scheme_current
-powercfg /setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 25dfa149-5dd1-4736-b5ab-e8a37b5b8187 0
+powercfg -setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 25dfa149-5dd1-4736-b5ab-e8a37b5b8187 0
 Powercfg -setactive scheme_current
 :: Disable Idle States
-powercfg /setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 abfc2519-3608-4c2a-94ea-171b0ed546ab 0
+powercfg -setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 abfc2519-3608-4c2a-94ea-171b0ed546ab 0
 Powercfg -setactive scheme_current
-powercfg /setdcvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 abfc2519-3608-4c2a-94ea-171b0ed546ab 0
+powercfg -setdcvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 abfc2519-3608-4c2a-94ea-171b0ed546ab 0
 Powercfg -setactive scheme_current
-PowerCfg /SETACVALUEINDEX SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 0
+PowerCfg -SETACVALUEINDEX SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 0
 Powercfg -setactive scheme_current
-PowerCfg /SETDCVALUEINDEX SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 0
+PowerCfg -SETDCVALUEINDEX SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 0
 Powercfg -setactive scheme_current
 :: Disable Hybrid Sleep
-powercfg /setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 0
-powercfg /setdcvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 0
-
+powercfg -setacvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 0
+powercfg -setdcvalueindex 95533644-e700-4a79-a56c-a89e8cb109d9 238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 0
 powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-
 powercfg.exe -setactive SCHEME_CURRENT
-
 ::DISABLE HIBERNATE
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v "HiberbootEnabled" /t REG_DWORD /d "0" /f
 powercfg /x -hibernate-timeout-ac 0
 powercfg /x -hibernate-timeout-dc 0
+::REG MODS
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\0012ee47-9041-4b5d-9b77-535fba8b1442\6738e2c4-e8a5-4a42-b16a-e040e769756e" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\238c9fa8-0aad-41ed-83f4-97be242c8f20\94ac6d29-73ce-41a6-809f-6363ba21b47e" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\238c9fa8-0aad-41ed-83f4-97be242c8f20\bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\245d8541-3943-4422-b025-13a784f679b7" /v "AcSettingIndex" /t reg_dword /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\2a737441-1930-4402-8d77-b2bebba308a3\0853a681-27c8-4100-a2fd-82013e970683" /v "AcSettingIndex" /t reg_dword /d "10000" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\2a737441-1930-4402-8d77-b2bebba308a3\48e6b7a6-50f5-4782-a5d4-53bb8f07e226" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\2a737441-1930-4402-8d77-b2bebba308a3\d4e98f31-5ffe-4ce1-be31-1b38b384c009" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\4f971e89-eebd-4455-a8de-9e59040e7347\96996bc0-ad50-47ec-923b-6f41874dd9eb" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583" /v "AcSettingIndex" /t reg_dword /d "100" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\54533251-82be-4824-96c1-47b60b740d00\3b04d4fd-1cc7-4f23-ab1c-d1337819c4bb" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\54533251-82be-4824-96c1-47b60b740d00\5d76a2ca-e8c0-402f-a133-2158492d58ad" /v "AcSettingIndex" /t reg_dword /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\54533251-82be-4824-96c1-47b60b740d00\893dee8e-2bef-41e0-89c6-b55d0929964c" /v "AcSettingIndex" /t reg_dword /d "100" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\54533251-82be-4824-96c1-47b60b740d00\bc5038f7-23e0-4960-96da-33abaf5935ec" /v "AcSettingIndex" /t reg_dword /d "100" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\7516b95f-f776-4464-8c53-06167f40cc99\3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e" /v "AcSettingIndex" /t reg_dword /d "0" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\Powerschemes\95533644-e700-4a79-a56c-a89e8cb109d9\7516b95f-f776-4464-8c53-06167f40cc99\aded5e82-b909-4619-9949-f5d71dac0bcb" /v "AcSettingIndex" /t reg_dword /d "100" /f
 
 ::DISABLE SYSMAIN/SUPERFETCH
 sc stop SysMain
