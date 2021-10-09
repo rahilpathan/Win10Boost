7742c69a214b027aab2f8ed4d3e1268e6e3a2842
Rahil Pathan <rahilpathan56@gmail.com>
2021-10-09 09:21:27 -0400

    Update Windows11-workaround.cmd

--- Other Tricks/Windows11-workaround.cmd
+++ Other Tricks/Windows11-workaround.cmd
@@ -8,21 +8,8 @@ reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassRAMCheck" /f /t REG_DWORD /d "1"
 reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassTPMCheck" /f /t REG_DWORD /d "1"
 reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassSecureBootCheck" /f /t REG_DWORD /d "1"
 
-echo add "BypassCPUCheck" 
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassCPUCheck" /f /t REG_DWORD /d "1"
-
-echo add "BypassStorageCheck"
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassStorageCheck" /f /t REG_DWORD /d "1"
-
-echo add "BypassRAMCheck"
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /f /t REG_DWORD /d "1"
-
-echo add "BypassTPMCheck"
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /f /t REG_DWORD /d "1"
-
-echo add "BypassSecureBootCheck"
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /f /t REG_DWORD /d "1"
-
-
-reg add "HKLM\SOFTWARE\Microsoft\WindowsSelfHost\UI\Selection" /v "UIContentType" /f /t REG_SZ /d "Mainline"
-
