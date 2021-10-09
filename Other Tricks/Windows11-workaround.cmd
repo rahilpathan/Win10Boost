1a1610f1ab9a08308b5916960787c3456b78b619
Rahil Pathan <rahilpathan56@gmail.com>
2021-10-09 09:20:30 -0400

    Update Windows11-workaround.cmd

--- Other Tricks/Windows11-workaround.cmd
+++ Other Tricks/Windows11-workaround.cmd
@@ -1,6 +1,12 @@
 ::WINDOWS 11 Bypass CPU Requirements
-echo add "AllowUpgradesWithUnsupportedTPMOrCPU" 
+@echo off 
+echo add "Modify MoSetup" 
 reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /f /t REG_DWORD /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassCPUCheck" /f /t REG_DWORD /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassStorageCheck" /f /t REG_DWORD /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassRAMCheck" /f /t REG_DWORD /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassTPMCheck" /f /t REG_DWORD /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "BypassSecureBootCheck" /f /t REG_DWORD /d "1"
 
 echo add "BypassCPUCheck" 
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassCPUCheck" /f /t REG_DWORD /d "1"
@@ -16,3 +22,7 @@ reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /f /t REG_DWORD /d "1"
 
 echo add "BypassSecureBootCheck"
 reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /f /t REG_DWORD /d "1"
+
+
+reg add "HKLM\SOFTWARE\Microsoft\WindowsSelfHost\UI\Selection" /v "UIContentType" /f /t REG_SZ /d "Mainline"
+
