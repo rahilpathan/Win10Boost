553c983a33dd16e0105a2317c57e30ad898d00c0
Rahil Pathan <rahilpathan56@gmail.com>
2024-12-21 00:00:33 -0500

    Add files via upload

---
+++ MaximPower.bat
@@ -0,0 +1,59 @@
+@echo off
+setlocal enabledelayedexpansion
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 88888888-8888-8888-8888-228888888821
+powercfg -setactive 88888888-8888-8888-8888-228888888821
+powercfg -changename 88888888-8888-8888-8888-228888888821 "Github-Rahilpathan" "Maximized performance settings for power users"
+
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 238c9fa8-0aad-41ed-83f4-97be242c8f20 29f6c1db-86da-48c5-9fdb-f2b67b1f44da 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 5d76a2ca-e8c0-402f-a133-2158492d58ad 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 68afb2d9-ee95-47a8-8f50-4115088073b1 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 100
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 be337238-0d82-4146-a960-4f3749d470c7 2
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 5fb4938d-1ee8-4b0f-9a3c-5036b0ab995c dd848b2a-8a5d-4451-9ae2-39cd41658f6c 2
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 7516b95f-f776-4464-8c53-06167f40cc99 fbd9aa66-9553-4097-ba44-ed6e9d65eab8 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 238c9fa8-0aad-41ed-83f4-97be242c8f20 29f6c1db-86da-48c5-9fdb-f2b67b1f44da 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 5d76a2ca-e8c0-402f-a133-2158492d58ad 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 68afb2d9-ee95-47a8-8f50-4115088073b1 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 100
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 be337238-0d82-4146-a960-4f3749d470c7 2
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 0
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 5fb4938d-1ee8-4b0f-9a3c-5036b0ab995c dd848b2a-8a5d-4451-9ae2-39cd41658f6c 2
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 7516b95f-f776-4464-8c53-06167f40cc99 fbd9aa66-9553-4097-ba44-ed6e9d65eab8 0
+powercfg -setacvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 94D3A615-A899-4AC5-AE2B-E4D8F634367F 1
+powercfg -setdcvalueindex 88888888-8888-8888-8888-228888888821 54533251-82be-4824-96c1-47b60b740d00 94D3A615-A899-4AC5-AE2B-E4D8F634367F 1
+
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583" /v Attributes /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\45bcc044-d885-43e2-8605-ee0ec6e96b59" /v Attributes /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Processor" /v AllowPState0 /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Processor" /v ProcessorThrottlingDisabled /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v CsEnabled /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v EnergyEstimationEnabled /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v MemoryPowerSavingMode /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v PowerLoggingEnabled /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583" /v ValueMax /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583" /v ValueMin /t REG_DWORD /d 0 /f
+
+bcdedit /deletevalue useplatformclock
+bcdedit /set disabledynamictick yes
+bcdedit /set useplatformtick yes
+powercfg /hibernate off
+powercfg -setactive 88888888-8888-8888-8888-228888888821
+
+echo Configuration complete! Your system is now set for maximum performance. I am not responsible damage to your system if you use this script or any part of this script.
+
+echo IMPORTANT NOTES:
+echo 1. System may run hotter than usual. Monitor your CPU temperatures carefully
+echo 2. If its a laptop. Battery life will significantly reduce.
+echo Enjoy!
+pause
