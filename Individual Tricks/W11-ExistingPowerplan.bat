a33f9418e5c4e1cb8021ef640a1efd7e1686893e
Rahil Pathan <rahilpathan56@gmail.com>
2025-05-16 00:19:55 -0400

    Update W11-ExistingPowerplan.bat

--- Individual Tricks/W11-ExistingPowerplan.bat
+++ Individual Tricks/W11-ExistingPowerplan.bat
@@ -10,60 +10,99 @@ if %errorlevel% neq 0 (
 )
 
 :: Retrieve the GUID of the current active power scheme
-for /f "tokens=4" %%A in ('powercfg /getactivescheme') do (
-    set "CURRENT_GUID=%%A"
+for /f "tokens=5" %%A in ('powercfg /getactivescheme') do (
+    set "CURRENT_name=%%A"
 )
+echo [*] Modifying current power plan: !CURRENT_name!
+echo ONLY continue if you are sure to modify this plan, otherwise exit NOW
+PAUSE
 
-echo [*] Modifying current power plan: !CURRENT_GUID!
+:: AC Settings
+powercfg -attributes SUB_PROCESSOR 36687f9e-e3a5-4dbf-b1dc-15eb381c6863 -ATTRIB_HIDE
+powercfg -attributes SUB_PROCESSOR 45bcc044-d885-43e2-8605-ee0ec6e96b59 -ATTRIB_HIDE
+powercfg -attributes SUB_PROCESSOR be337238-0d82-4146-a960-4f3749d470c7 -ATTRIB_HIDE
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR 36687f9e-e3a5-4dbf-b1dc-15eb381c6863 5
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR 45bcc044-d885-43e2-8605-ee0ec6e96b59 100
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR 94d3a615-a899-4ac5-ae2b-e4d8f634367f 1
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR be337238-0d82-4146-a960-4f3749d470c7 6
+powercfg /setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 5
+powercfg /setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
+powercfg /setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 100
+powercfg /setacvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 5d76a2ca-e8c0-402f-a133-2158492d58ad 0
+powercfg /setacvalueindex SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powercfg /setacvalueindex SCHEME_CURRENT 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df6 0
+powercfg /setacvalueindex SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+powercfg /setacvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 17aaa29b-8b43-4b94-aafe-35f64daaf1ee 0
+powercfg /setacvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 aded5e82-b909-4619-9949-f5d71dac0bcb 0
+powercfg /setacvalueindex SCHEME_CURRENT 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN 5
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 100
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 1
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PARKINGMAXCORES 100
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PARKINGMINCORES 100
+powercfg /setacvalueindex SCHEME_CURRENT SUB_DISK DISKIDLE 1000
+powercfg /setacvalueindex SCHEME_CURRENT SUB_DISK DISKBUSYTHRESHOLD 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_DISK IDLETIMEOUT 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PCIEXPRESS ASPM 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_USB USBSELECTIVE 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOIDLE 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_VIDEO ADAPTBRIGHT 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_WIFI POWERMODE 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PCIEXPRESS PCIEASPM 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEODIM 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_WIFI WIFIPOWER 0
+powercfg /setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PERFBOOSTMODE 6
 
-:: Advanced AC Settings
-powercfg -setacvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 100
-powercfg -setacvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
-powercfg -setacvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 be337238-0d82-4146-a960-4f3749d470c7 2
-powercfg -setacvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 100
-powercfg -setacvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 5d76a2ca-e8c0-402f-a133-2158492d58ad 1
-powercfg -setacvalueindex !CURRENT_GUID! 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
-powercfg -setacvalueindex !CURRENT_GUID! 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df6 0
-powercfg -setacvalueindex !CURRENT_GUID! 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
-powercfg -setacvalueindex !CURRENT_GUID! 7516b95f-f776-4464-8c53-06167f40cc99 17aaa29b-8b43-4b94-aafe-35f64daaf1ee 0
-powercfg -setacvalueindex !CURRENT_GUID! 7516b95f-f776-4464-8c53-06167f40cc99 aded5e82-b909-4619-9949-f5d71dac0bcb 0
-powercfg -setacvalueindex !CURRENT_GUID! 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 0
-powercfg -setacvalueindex !CURRENT_GUID! SUB_PROCESSOR PROCTHROTTLEMAX 100
-powercfg -setacvalueindex !CURRENT_GUID! SUB_PROCESSOR PROCTHROTTLEMIN 100
-powercfg -setacvalueindex !CURRENT_GUID! SUB_DISK DISKIDLE 0
-powercfg -setacvalueindex !CURRENT_GUID! SUB_PCIEXPRESS PCIEASPM 0
-powercfg -setacvalueindex !CURRENT_GUID! SUB_USB USBSELECTIVE 0
-powercfg -setacvalueindex !CURRENT_GUID! SUB_VIDEO VIDEOIDLE 0
-powercfg -setacvalueindex !CURRENT_GUID! SUB_VIDEO VIDEODIM 0
-powercfg -setacvalueindex !CURRENT_GUID! SUB_WIFI WIFIPOWER 0
 
-:: Advanced DC Settings
-powercfg -setdcvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 100
-powercfg -setdcvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 30
-powercfg -setdcvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 be337238-0d82-4146-a960-4f3749d470c7 1
-powercfg -setdcvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 50
-powercfg -setdcvalueindex !CURRENT_GUID! 54533251-82be-4824-96c1-47b60b740d00 5d76a2ca-e8c0-402f-a133-2158492d58ad 0
-powercfg -setdcvalueindex !CURRENT_GUID! 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 1200
-powercfg -setdcvalueindex !CURRENT_GUID! 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df6 1
-powercfg -setdcvalueindex !CURRENT_GUID! 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 1
-powercfg -setdcvalueindex !CURRENT_GUID! 7516b95f-f776-4464-8c53-06167f40cc99 17aaa29b-8b43-4b94-aafe-35f64daaf1ee 60
-powercfg -setdcvalueindex !CURRENT_GUID! 7516b95f-f776-4464-8c53-06167f40cc99 aded5e82-b909-4619-9949-f5d71dac0bcb 1
-powercfg -setdcvalueindex !CURRENT_GUID! 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 2
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_PROCESSOR PROCTHROTTLEMAX 100
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_PROCESSOR PROCTHROTTLEMIN 30
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_DISK DISKIDLE 1200
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_PCIEXPRESS PCIEASPM 1
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_USB USBSELECTIVE 1
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_VIDEO VIDEOIDLE 60
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_VIDEO VIDEODIM 1
-powercfg -setdcvalueindex !CURRENT_GUID! SUB_WIFI WIFIPOWER 2
+:: DC Settings
+powercfg /setdcvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 2
+powercfg /setdcvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
+powercfg /setdcvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 be337238-0d82-4146-a960-4f3749d470c7 1
+powercfg /setdcvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 ea062031-0e34-4ff1-9b6d-eb1059334028 100
+powercfg /setdcvalueindex SCHEME_CURRENT 54533251-82be-4824-96c1-47b60b740d00 5d76a2ca-e8c0-402f-a133-2158492d58ad 0
+powercfg /setdcvalueindex SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 1200
+powercfg /setdcvalueindex SCHEME_CURRENT 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df6 1
+powercfg /setdcvalueindex SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 1
+powercfg /setdcvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 17aaa29b-8b43-4b94-aafe-35f64daaf1ee 60
+powercfg /setdcvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 aded5e82-b909-4619-9949-f5d71dac0bcb 1
+powercfg /setdcvalueindex SCHEME_CURRENT 19cbb8fa-5279-450e-9fac-8a3d5fedd0c1 12bbebe6-58d6-4636-95bb-3217ef867c1a 2
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN 5
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 100
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 1
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PARKINGMAXCORES 100
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PARKINGMINCORES 2
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_DISK DISKIDLE 1000
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PCIEXPRESS ASPM 1
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_USB USBSELECTIVE 1
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOIDLE 60
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_VIDEO ADAPTBRIGHT 1
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_WIFI POWERMODE 2
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE 900
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE 1800
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PCIEXPRESS PCIEASPM 1
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_VIDEO VIDEODIM 300
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_WIFI WIFIPOWER 2
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PERFBOOSTMODE 1
 
 :: Apply registry tweaks for performance optimization
+sc stop "SysMain" >nul 2>&1
+sc config "SysMain" start= disabled  >nul 2>&1
+bcdedit /deletevalue useplatformclock  >nul 2>&1
+bcdedit /set disabledynamictick yes  >nul 2>&1
+bcdedit /set tscsyncpolicy Enhanced  >nul 2>&1
+bcdedit /set useplatformtick yes  >nul 2>&1
+fsutil behavior set memoryusage 2  >nul 2>&1
+fsutil behavior set allowextchar 1  >nul 2>&1
+fsutil behavior set mftzone 4  >nul 2>&1
+fsutil behavior set disablecompression 1  >nul 2>&1
+fsutil behavior set disableencryption 1  >nul 2>&1
+fsutil behavior set disabledeletenotify 0  >nul 2>&1
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling" /v "PowerThrottlingOff" /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "EnergyEstimationEnabled" /t REG_DWORD /d 0 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "PlatformAoAcOverride" /t REG_DWORD /d 0 /f
-sc config "SysMain" start= disabled
-sc stop "SysMain" >nul 2>&1
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power" /v "CSEnabled" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "SystemResponsiveness" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d 4294967295 /f
@@ -78,8 +117,8 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DefaultTTL
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "DisableTaskOffload" /t REG_DWORD /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v "MenuShowDelay" /t REG_SZ /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v "AutoEndTasks" /t REG_SZ /d 1 /f
-bcdedit /set disabledynamictick yes >nul 2>&1
-bcdedit /set useplatformtick yes >nul 2>&1
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v SystemResponsiveness /t REG_DWORD /d 30 /f
 
 echo Power plan updated successfully!
 PAUSE
